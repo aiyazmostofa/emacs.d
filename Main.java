26542d1f85b11f185b3cf791359f12bc152ea35e
Aiyaz Mostofa <aiyazmostofa@tuta.io>
2023-12-20 22:55:21 -0600

    refactored main.java

--- Main.java
+++ Main.java
@@ -5,60 +5,56 @@ import java.math.*;
 // %s
 public class Main {
     void run() {
-
     }
 
-    static final String INPUT = "%s";
-    static final String OUTPUT = "%s";
-    final FastScanner in;
-    final PrintWriter out;
+    static String IN = "%s";
+    static String OUT = "%s";
+    Kattio in;
+    PrintWriter out;
 
     Main(InputStream input, OutputStream output) {
-        in = new FastScanner(input);
+        in = new Kattio(input);
         out = new PrintWriter(output);
     }
 
-    void close() {
-        out.close();
-    }
-
-    public static void main(String[] args) throws Exception {
-        InputStream input = INPUT.length() == 0 || System.getenv("CP_ENJOYER") != null ? System.in
-                : new FileInputStream(INPUT);
-        OutputStream output = OUTPUT.length() == 0 || System.getenv("CP_ENJOYER") != null ? System.out
-                : new FileOutputStream(OUTPUT);
-        Main main = new Main(input, output);
+    public static void main(String[] args) throws IOException {
+        Main main = new Main(
+                IN.length() == 0 || System.getenv("CP_ENJOYER") != null ? System.in
+                        : new FileInputStream(IN),
+                OUT.length() == 0 || System.getenv("CP_ENJOYER") != null ? System.out
+                        : new FileOutputStream(OUT));
         main.run();
-        main.close();
+        main.in.br.close();
+        main.out.close();
     }
 
-    class FastScanner {
-        private BufferedReader bufferedReader;
-        private StringTokenizer stringTokenizer;
+    class Kattio {
+        BufferedReader br;
+        StringTokenizer st;
 
-        public FastScanner(InputStream inputStream) {
-            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        Kattio(InputStream is) {
+            br = new BufferedReader(new InputStreamReader(is));
         }
 
-        public String next() {
+        String next() {
             try {
-                while (stringTokenizer == null || !stringTokenizer.hasMoreTokens())
-                    stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-                return stringTokenizer.nextToken();
+                while (st == null || !st.hasMoreTokens())
+                    st = new StringTokenizer(br.readLine());
+                return st.nextToken();
             } catch (Exception ignored) {
             }
             return null;
         }
 
-        public int nextInt() {
+        int nextInt() {
             return Integer.parseInt(next());
         }
 
-        public long nextLong() {
+        long nextLong() {
             return Long.parseLong(next());
         }
 
-        public double nextDouble() {
+        double nextDouble() {
             return Double.parseDouble(next());
         }
     }
