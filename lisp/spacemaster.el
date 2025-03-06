dc196df3555bce3612ad02a36ac9b1d5986c18d8
Aiyaz Mostofa <aiyaz5040@gmail.com>
2025-03-06 14:44:19 -0600

    make escape into tab, escape is immediate

--- lisp/spacemaster.el
+++ lisp/spacemaster.el
@@ -22,12 +22,20 @@
    (t
     (setq spacemaster--buffer (concat spacemaster--buffer " C-")))))
 
-(defun spacemaster--handle-escape ()
-  "Handle escape key being pressed in spacemaster."
-  (if (string-match-p "C-\\'" spacemaster--buffer)
-      (setq spacemaster--buffer
-            (concat (substring spacemaster--buffer nil -2) "M-"))
-    (setq spacemaster--buffer "")))
+(defun spacemaster--handle-tab ()
+  "Handle tab key being pressed in spacemaster."
+  (cond
+   ((string-match-p "C-\\'" spacemaster--buffer)
+    (setq spacemaster--buffer
+          (concat (substring spacemaster--buffer nil -2) "M-")))
+   ((string-match-p "C-M-\\'" spacemaster--buffer)
+    (setq spacemaster--buffer
+          (concat (substring spacemaster--buffer nil -4) "C-")))
+   ((string-match-p "M-\\'" spacemaster--buffer)
+    (setq spacemaster--buffer
+          (concat (substring spacemaster--buffer nil -2) "C-M-")))
+   (t
+    (setq spacemaster--buffer ""))))
 
 (defun spacemaster--handle-misc ()
   "Handle any character not specially handled by spacemaster."
@@ -39,16 +47,16 @@
 (defun spacemaster--process-command ()
   "See if a command can be executed from our current keybinding buffer.
 If it cannot, handle accordingly."
-  (setq spacemaster--candidate-function
-        (key-binding (kbd spacemaster--buffer)))
-  (cond
-   ((string-match-p "C-\\'\\|M-\\'" spacemaster--buffer))
-   ((not spacemaster--candidate-function)
-    (spacemaster--echo "Invalid key sequence")
-    (setq spacemaster--buffer ""))
-   ((not (keymapp spacemaster--candidate-function))
-    (call-interactively spacemaster--candidate-function)
-    (setq spacemaster--buffer ""))))
+  (unless (string-match-p "C-\\'\\|M-\\'" spacemaster--buffer)
+    (setq spacemaster--candidate-function
+          (key-binding (kbd spacemaster--buffer)))
+    (cond
+     ((not spacemaster--candidate-function)
+      (spacemaster--echo "Invalid key sequence")
+      (setq spacemaster--buffer ""))
+     ((not (keymapp spacemaster--candidate-function))
+      (call-interactively spacemaster--candidate-function)
+      (setq spacemaster--buffer "")))))
 
 (defun spacemaster ()
   "The master command that temporarily takes control of the keyboard.
@@ -57,10 +65,10 @@ Here are some example keybinding conversions:
 'SPC a' -> 'C-a'
 'SPC a SPC b' -> 'C-a C-b'
 'SPC a b' -> 'C-a b'
-'SPC a SPC ESC b' -> 'C-a M-b'
-'SPC ESC a' -> 'M-a'
-'SPC ESC a b' -> 'M-a b'
-'SPC ESC a SPC b' -> 'M-a C-b'
+'SPC a SPC TAB b' -> 'C-a M-b'
+'SPC TAB a' -> 'M-a'
+'SPC TAB a b' -> 'M-a b'
+'SPC TAB a TAB b' -> 'M-a C-b'
 "
   (interactive)
   (setq spacemaster--buffer "C-")
@@ -71,7 +79,9 @@ Here are some example keybinding conversions:
      ((eq spacemaster--char 32)
       (spacemaster--handle-space))
      ((eq spacemaster--char 27)
-      (spacemaster--handle-escape))
+      (setq spacemaster--buffer ""))
+     ((eq spacemaster--char 9)
+      (spacemaster--handle-tab))
      (t
       (spacemaster--handle-misc)))
     (spacemaster--echo "%s" spacemaster--buffer)
