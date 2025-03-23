edda20605da62e0d6eb0aa8f1e57d3bcdbd649dc
Aiyaz Mostofa <aiyaz5040@gmail.com>
2025-03-23 04:47:26 -0500

    gruvbox throwback to old times

--- init.el
+++ init.el
@@ -42,10 +42,16 @@
 (global-display-line-numbers-mode)
 
 ;; Setup themes ef-themes
+;(use-package
+; ef-themes
+; :ensure t
+; :config (ef-themes-select 'ef-melissa-dark))
+
+;; Setup doom themes
 (use-package
- ef-themes
+ doom-themes
  :ensure t
- :config (ef-themes-select 'ef-melissa-dark))
+ :config (load-theme 'doom-gruvbox :no-confirm))
 
 ;; Setup spacious padding
 (use-package
