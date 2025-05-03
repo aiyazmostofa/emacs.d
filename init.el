16df499f2f88a96719204f784bff4458c349fccb
Aiyaz Mostofa <aiyaz5040@gmail.com>
2025-05-02 22:06:56 -0500

    should be done for now

--- init.el
+++ init.el
@@ -224,21 +224,29 @@ If the buffer associated with the window is not in any other window, kill it too
      (elisp-autofmt-buffer)))
   ("C-c D" . xref-find-definitions) ("C-c R" . xref-find-references)))
 
-;; buffer name
-(defun my-modeline--buffer-name ()
-  (format " %s " (buffer-name)))
-(defvar-local my-modeline-buffer-name
-    '(:eval (propertize (my-modeline--buffer-name) 'face 'bold)))
-(put 'my-modeline-buffer-name 'risky-local-variable t)
 
 ;; evil state
 (defface evil-normal-state-face '((t))
+  "bjksldf")
+(defface evil-normal-state-face-a '((t))
   "bjksldf")
 (ef-themes-with-colors
  (set-face-attribute 'evil-normal-state-face nil
                      :background fg-mode-line
                      :foreground bg-mode-line))
-(force-mode-line-update)
+(ef-themes-with-colors
+ (set-face-attribute
+  'evil-normal-state-face-a
+  nil
+  :background fg-dim
+  :foreground bg-dim))
+;; buffer name
+(defun my-modeline--buffer-name ()
+  (format " %s " (buffer-name)))
+(defvar-local my-modeline-buffer-name
+
+    '(:eval (propertize (my-modeline--buffer-name) 'face 'bold)))
+(put 'my-modeline-buffer-name 'risky-local-variable t)
 (defun my-modeline--evil-state ()
   (format " <%s> "
           (cond
@@ -248,25 +256,40 @@ If the buffer associated with the window is not in any other window, kill it too
             "R")
            ((evil-motion-state-p)
             "M")
-           ((evil-visual-state-p)
-            "V")
            ((evil-operator-state-p)
             "O")
+           ((evil-visual-state-p)
+            "V")
            ((evil-insert-state-p)
             "I")
            (t
             "E"))))
 (defvar-local my-modeline-evil-state
     '(:eval
-      (propertize (my-modeline--evil-state)
-                  'face
-                  'evil-normal-state-face)))
+      (if (mode-line-window-selected-p)
+          (propertize (my-modeline--evil-state)
+                      'face
+                      'evil-normal-state-face)
+        (propertize (my-modeline--evil-state)
+                    'face
+                    'evil-normal-state-face-a))))
 (put 'my-modeline-evil-state 'risky-local-variable t)
+
+;; minor modes
+(defun my-modeline--minor-mode-name ()
+  "Return capitalized `minor-mode' as a string."
+  (if (bound-and-true-p eglot--managed-mode)
+      (format "[LSP] " (capitalize (symbol-name major-mode)))
+    " "))
+(defvar-local my-modeline-minor-mode
+    '(:eval (my-modeline--minor-mode-name))
+  "Mode line construct to display the major mode.")
+(put 'my-modeline-minor-mode 'risky-local-variable t)
 
 ;; major mode
 (defun my-modeline--major-mode-name ()
   "Return capitalized `major-mode' as a string."
-  (format " (%s) " (capitalize (symbol-name major-mode))))
+  (format "(%s)" (capitalize (symbol-name major-mode))))
 (defvar-local my-modeline-major-mode
     '(:eval (my-modeline--major-mode-name))
   "Mode line construct to display the major mode.")
@@ -276,7 +299,8 @@ If the buffer associated with the window is not in any other window, kill it too
                 my-modeline-evil-state
                 my-modeline-buffer-name
                 mode-line-format-right-align
-                my-modeline-major-mode))
+                my-modeline-major-mode
+                my-modeline-minor-mode))
 ;; Setup eshell keybinding
 (defun eshell-buffer-p (buffer)
   (with-current-buffer buffer
