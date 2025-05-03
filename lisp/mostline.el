fb1f9c2e486f65282aaeb6659f71e41eaeecc1b6
Aiyaz Mostofa <aiyaz5040@gmail.com>
2025-05-03 14:52:41 -0500

    finished maybe?

--- lisp/mostline.el
+++ lisp/mostline.el
@@ -1,77 +1,77 @@
 ;;; mostline.el --- The worst mode line. -*- lexical-binding: t -*-
-(defface mostline--state-active-face '((t))
-  "Face for state indicator when the buffer is selected.")
-(defface mostline--state-disabled-face '((t))
-  "Face for state indicator when the buffer is NOT selected.")
-(ef-themes-with-colors
- (set-face-attribute
-  'mostline--state-active-face
-  nil
-  :background fg-mode-line
-  :foreground bg-mode-line)
- (set-face-attribute
-  'mostline--state-disabled-face
-  nil
-  :background fg-dim
-  :foreground bg-dim))
+;; State indicator (for evil)
 (defun mostline--state ()
-  (format " <%s> "
-          (cond
-           ((not (bound-and-true-p evil-mode))
-            "E")
-           ((evil-normal-state-p)
-            "N")
-           ((evil-replace-state-p)
-            "R")
-           ((evil-motion-state-p)
-            "M")
-           ((evil-operator-state-p)
-            "O")
-           ((evil-visual-state-p)
-            "V")
-           ((evil-insert-state-p)
-            "I")
-           (t
-            "E"))))
+  "Computes the text for the state indicator."
+  (cond
+   ((not (bound-and-true-p evil-mode))
+    "")
+   ((evil-normal-state-p)
+    " <N> ")
+   ((evil-replace-state-p)
+    " <R> ")
+   ((evil-motion-state-p)
+    " <M> ")
+   ((evil-operator-state-p)
+    " <O> ")
+   ((evil-visual-state-p)
+    " <V> ")
+   ((evil-insert-state-p)
+    " <I> ")
+   (t
+    " <E> ")))
 (defvar-local mostline-state
     '(:eval
       (propertize (mostline--state)
                   'face
                   (if (mode-line-window-selected-p)
-                      'mostline--state-active-face
-                    'mostline--state-disabled-face))))
+                      `(:background
+                        ,(face-attribute 'mode-line :foreground)
+                        :foreground
+                        ,(face-attribute 'mode-line :background))
+                    `(:background
+                      ,(face-attribute
+                        'mode-line-inactive
+                        :foreground)
+                      :foreground
+                      ,(face-attribute
+                        'mode-line-inactive
+                        :background)))))
+  "Computes the text+style for the state indicator.")
 (put 'mostline-state 'risky-local-variable t)
 
 (defun mostline--buffer ()
+  "Computes the text for the buffer name."
   (format " %s " (buffer-name)))
 (defvar-local mostline-buffer
-    '(:eval (propertize (mostline--buffer) 'face 'bold)))
+    '(:eval (propertize (mostline--buffer) 'face 'bold))
+  "Computes the text+style for the buffer name.")
 (put 'mostline-buffer 'risky-local-variable t)
 
 (defun mostline--saved ()
+  "Computes the text for whether the current buffer is saved."
   (if (and buffer-file-name (buffer-modified-p))
       "*"
     ""))
 
 (defvar-local mostline-saved
     '(:eval (propertize (mostline--saved) 'face 'error))
-  "What is going on")
+  "Computes the text+style for whether the current buffer is saved.")
 (put 'mostline-saved 'risky-local-variable t)
 
 (defun mostline--major-mode ()
-  "Return capitalized `major-mode' as a string."
+  "Computes the text for whether the current buffer's major mode."
   (format "(%s)" (capitalize (symbol-name major-mode))))
 (defvar-local mostline-major-mode '(:eval (mostline--major-mode))
-  "Mode line construct to display the major mode.")
+  "Computes the text+style for whether the current buffer's major mode.")
 (put 'mostline-major-mode 'risky-local-variable t)
 
 (defun mostline--lsp ()
-  "Return capitalized `minor-mode' as a string."
+  "Computes the text for whether the current buffer is using eglot."
   (if (bound-and-true-p eglot--managed-mode)
       (format "[LSP] " (capitalize (symbol-name major-mode)))
     " "))
 (defvar-local mostline-lsp '(:eval (mostline--lsp))
-  "Mode line construct to display the major mode.")
+  "Computes the text+style for whether the current buffer is using eglot.")
 (put 'mostline-lsp 'risky-local-variable t)
 
 (defvar mostline-format
@@ -81,7 +81,9 @@
     mostline-saved
     mode-line-format-right-align
     mostline-major-mode
-    mostline-lsp))
+    mostline-lsp)
+  "The main entrypoint to use mostline.
+ Set the `mode-line-format' variable using `setq-default'.")
 
 (provide 'mostline)
 ;;; mostline.el ends here
